ed9fc0e447bbefcfb6c0f0b20ae81d174f66498a
Andrew Lyasoff <andrew.lyasoff@gmail.com>
2019-04-14 11:02:06 -0400

    Add files via upload

---
+++ LS-pureIOU-grid2000-plots.jl
@@ -0,0 +1,85 @@
+include("LS-method-7s-init.jl");
+
+begin
+    @load "LS_pureIOU_2000_saved.jld2" LS_pureIOU_2000_saved;
+    r_list=LS_pureIOU_2000_saved[1]
+    a_list=LS_pureIOU_2000_saved[2]
+    Λ1=LS_pureIOU_2000_saved[3]
+    tbl2=LS_pureIOU_2000_saved[4]
+end;
+
+## Two important checkpoints!
+sum(Λ1) # should print 1
+[sum(Λ1[i,:]) for i=1:nos]-BSP # should print a list of zeros
+
+begin
+    A=hcat(r_list,a_list)
+    Aplus=A[A[:,2].>0,:]
+    Aminus=A[A[:,2].<=0,:]
+    iloc1=findmin(Aplus[:,2])[2]
+    iloc2=findmax(Aminus[:,2])[2]
+end;
+
+#the nearest interest rates
+Aplus[iloc1,1],Aminus[iloc2,1]
+
+#the respective aggregate demand
+Aplus[iloc1,2],Aminus[iloc2,2]
+
+#the gap in the demand a(r)
+Aplus[iloc1,2]-Aminus[iloc2,2]
+
+#the gap in the interest rate is
+Aplus[iloc1,1]-Aminus[iloc2,1]
+
+begin
+    scatter(a_list,r_list,label="",color=:black,border=true)
+    plot!((0.02:0.0001:0.0305)*0,0.02:0.0001:0.0305,color=:black,label="")
+    xlabel!("average assets")
+    ylabel!("interest rate")
+end
+
+
+begin
+    r=r_list[end]
+    b=3.0;
+    minkap=-b;
+    if r<=0.0
+        minkap = -b;
+    else
+        minkap=-min(b, wage*hours[1]/r);
+    end;
+    maxkap=16.0;
+    Lg=2000; # The only place to set the grid size.
+    grda=[minkap+i*(maxkap-minkap)/(Lg-1) for i=0:(Lg-1)];
+    β=exp(-ρ);
+end;
+
+
+begin
+    bond_price=ι/(1+r)
+    cons=[(1+r)*grda[i]+wage*hours[k]-grda[Int64(tbl2[k,i])] for k=1:nos, i=1:Lg];
+    aassets=[grda[Int64(tbl2[k,i])] for k=1:nos, i=1:Lg];
+    shares=[grda[Int64(tbl2[k,i])]/bond_price for k=1:nos, i=1:Lg];
+end;
+
+begin
+    SL=[sum(Λ1[state,:]) for state=1:nos];
+    LF=[[Λ1[state,1]/SL[state]] for state=1:nos];
+    for i=2:Lg
+        for state=1:nos
+            LF[state]=vcat(LF[state],LF[state][end]+Λ1[state,i]/SL[state])
+        end
+    end
+end;
+
+
+begin
+    plot(aassets[1,2:920],LF[1][2:920],label="(LS)",color=:black)
+    plot!(aassets[2,2:920],LF[2][2:920],label="",color=:black)
+    plot!(aassets[3,2:920],LF[3][2:920],label="",color=:black)
+    plot!(aassets[4,2:920],LF[4][2:920],label="",color=:black)
+    plot!(aassets[5,2:920],LF[5][2:920],label="",color=:black)
+    plot!(aassets[6,2:920],LF[6][2:920],label="",color=:black)
+    plot!(aassets[7,2:920],LF[7][2:920],label="",color=:black)
+end
---
+++ LS-pureIOU-grid2000.jl
@@ -0,0 +1,79 @@
+#
+# A single execution of this file generates a single pair (a(r),r).
+# The pair is then stored in "LS_pureIOU_2000_saved.jld2", together with
+# all previously computed pairs.
+#      If running for the first time (no "LS_pureIOU_2000_saved.jld2" exists)
+#      uncomment lines 14-19. Put the #'s back for all subsequent runs.
+#
+include("LS-method-7s-init.jl");
+
+## Uncomment and execute the next block the very *first* time,
+## *only* if "LS_pureIOU_2000_saved.jld2" does not exist in the current path.
+## Put the #'s back for all subsequent runs.
+
+#begin
+#    a_list=Array{Float64}(undef, 0)
+#    r_list=Array{Float64}(undef, 0)
+#    LS_pureIOU_2000_saved=[r_list,a_list]
+#    @save "LS_pureIOU_2000_saved.jld2" LS_pureIOU_2000_saved
+#end;
+
+##
+## The next block guesses the next candidate for r.
+## It can be bypassed by setting r=<value> explicitly. See line 68.
+##
+
+begin
+    @load "LS_pureIOU_2000_saved.jld2" LS_pureIOU_2000_saved;
+    r_list=LS_pureIOU_2000_saved[1]
+    a_list=LS_pureIOU_2000_saved[2]
+    if length(r_list)==0
+        r=0.03
+        elseif length(r_list)==1
+        r=0.025
+        else 
+        r=0.5*(r_list[findall(x->x<0,a_list)[end]]+r_list[findall(x->x>0,a_list)[end]])
+    end
+    push!(r_list,r)
+end;
+
+
+begin
+    b=3.0;
+    minkap=-b;
+    if r<=0.0
+        minkap = -b;
+    else
+        minkap=-min(b, wage*hours[1]/r);
+    end;
+    maxkap=16.0;
+end
+
+begin
+    Lg=2000; # The only place to set the grid size.
+    grda=[minkap+i*(maxkap-minkap)/(Lg-1) for i=0:(Lg-1)];
+    β=exp(-ρ);
+end
+
+@time Λ1,tbl2=LS_method(r,5,5,nos,hours,wage,β,grda,Lg,CPROB,R);
+
+## Two important checkpoints!
+sum(Λ1) # should print 1
+[sum(Λ1[i,:]) for i=1:nos]-BSP # should print a list of zeros
+
+begin
+    policy=[grda[Int64(tbl2[kk,ii])] for kk=1:nos, ii=1:Lg]
+    a=sum(Λ1.*policy) # the aggregate demand a(r)
+end;
+
+##
+## If running with an explicit r (see line 22)
+## be sure that a_list and r_list exist before running the next block.
+##
+
+begin
+    a_list=push!(a_list,a)
+    LS_pureIOU_2000_saved=[r_list,a_list,Λ1,tbl2]
+    @save "LS_pureIOU_2000_saved.jld2" LS_pureIOU_2000_saved
+end;
+
